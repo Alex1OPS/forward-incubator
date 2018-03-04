59ddf3dc3d4b22a30b5bd5a2305619c665e5c5b1
Alex <ale51098223@yandex.ru>
2018-03-05 05:20:47 +0600

    Добавлен скрипт преднакачки для задания 1

--- tasks/lec_1/pre_schema.sql
+++ tasks/lec_1/pre_schema.sql
@@ -1,1 +1,16 @@
--- Скрипт создания таблиц и накачки данными
+п»ї-- РЎРєСЂРёРїС‚ СЃРѕР·РґР°РЅРёСЏ С‚Р°Р±Р»РёС† Рё РЅР°РєР°С‡РєРё РґР°РЅРЅС‹РјРё
+-- РґР»СЏ Р·Р°РґР°РЅРёСЏ 1
+create table fw_identifiers
+(
+  id_identifier         NUMBER(10) not null,
+  v_identifier_type     VARCHAR2(255),
+  v_value               VARCHAR2(128 CHAR) not null,
+  v_status              CHAR(1 CHAR) default 'N' not null,
+  b_deleted             NUMBER(1) default 0 not null,
+  v_ext_ident           VARCHAR2(20 CHAR)
+)
+/
+insert into fw_identifiers(id_identifier, v_identifier_type, v_value, v_status, b_deleted, v_ext_ident)
+  select 2000 + rownum, 'РўРёРї ' || trunc(dbms_random.value(1, 6)), '00801' || 1000 + rownum, 'N', 0, to_char(rownum)
+	from dual connect by level <= 100
+/
